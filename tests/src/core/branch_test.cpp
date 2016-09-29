3e50d845a30d832d9921b040b3dcd4bb9f45a2ea
Luis Rodriguez-Lujan <luis.rodriguezl@upm.es>
2016-09-29 15:51:48 +0200

    Added lots of tests for Branch partial fix #3

--- tests/src/core/branch_test.cpp
+++ tests/src/core/branch_test.cpp
@@ -1,19 +1,22 @@
 #include <unittest++/UnitTest++.h>
 #include "core/node.h"
 #include "core/branch.h"
+#include "core/neurite.h"
 
+SUITE(branch_tests){
 using namespace neurostr;
 
-TEST(branch_empty_constructor) {
+TEST(empty_constructor) {
   Branch b;
   CHECK_EQUAL(b.size(),0);
-  CHECK_EQUAL(b.has_root(), false);
+  CHECK(!b.has_root());
   CHECK_EQUAL(b.id().size(), 0);
   CHECK_EQUAL(b.order(), -1);
+  CHECK(!b.valid_neurite());
 }
 
 
-TEST(branch_partial_constructor) {
+TEST(partial_constructor) {
   Node r(1);
   Branch::id_type id{0,0,0};
   Branch b{id,1,r};
@@ -22,9 +25,10 @@ TEST(branch_partial_constructor) {
   CHECK_EQUAL(b.id().size(), 3);
   CHECK_EQUAL(b.order(), 1);
   CHECK(b.root()==r);
+  CHECK(!b.valid_neurite());
 }
 
-TEST(branch_full_constructor) {
+TEST(full_constructor) {
   Node r(1),a(2),c(3);
   std::vector<Node> nodes{a,c};
   Branch::id_type id{0,0,1};
@@ -36,12 +40,147 @@ TEST(branch_full_constructor) {
   CHECK(b.root()==r);
   CHECK(b.first()==a);
   CHECK(b.last()==c);
+  CHECK(!b.valid_neurite());
 }
 
-TEST(branch_push_back) {
-  Node r(1),a(2);
+TEST(order_set){
   Branch::id_type id{0,0,0};
-  Branch b{id,1,r};
+  Branch b{id,1};
+  
+  CHECK_EQUAL(b.order(),1);
+  b.order(2);
+  CHECK_EQUAL(b.order(),2);
+}
+
+TEST(id_set){
+  Branch::id_type id{0,0,0};
+  Branch::id_type id_b{0,0,1};
+  Branch b{id,1};
+  
+  for(unsigned int i = 0 ; i<id.size() ; ++i){
+    CHECK_EQUAL(b.id()[i],id[i]);
+  }
+  
+  b.id(id_b);
+  for(unsigned int i = 0 ; i<id.size() ; ++i){
+    CHECK_EQUAL(b.id()[i],id_b[i]);
+  }
+}
+
+TEST(id_string){
+  Branch::id_type id{1,2,3};
+  Branch::id_type id_b{};
+  Branch b{id,1};
+  
+  CHECK_EQUAL(b.idString(),std::string("1-2-3"));
+  
+  b.id(id_b);
+  CHECK_EQUAL(b.idString(),std::string());
+}
+
+TEST(set_neurite){
+  Neurite n(1);
+  Branch b{};
+  
+  CHECK(!b.valid_neurite());
+  
+  b.neurite(&n);
+  CHECK(b.valid_neurite());
+  CHECK_EQUAL(b.neurite(),n);
+}
+
+TEST(set_null_neurite){
+  Neurite n(1);
+  Branch b{};
+  
+  CHECK(!b.valid_neurite());
+  
+  b.neurite(&n);
+  CHECK(b.valid_neurite());
+  CHECK_EQUAL(b.neurite(),n);
+  
+  b.neurite(nullptr);
+  CHECK(!b.valid_neurite());
+}
+
+TEST(root_set_cp){
+  Node a(1);
+  Branch b{};
+  
+  CHECK(!b.has_root());
+  
+  // copy
+  b.root(a);
+  CHECK(b.has_root());
+  CHECK_EQUAL(b.root(),a);
+}
+
+TEST(root_set_mv){
+  Branch b{};
+  
+  CHECK(!b.has_root());
+  
+  // move (Rvalue)
+  b.root(Node(1));
+  CHECK(b.has_root());
+  CHECK_EQUAL(b.root(),Node(1));
+}
+
+// Access first
+TEST(access_first){
+  Node a(1),c(2);
+  Branch::id_type id{0,0,1};
+  Branch b{id,1};  
+  
+  // Try to access first -> error
+  CHECK_THROW(b.first(), std::logic_error);
+  
+  // Add node (cp)
+  b.push_back(a);
+  CHECK(b.first() == a);
+  
+  // add another node - dont change
+  b.push_back(c);
+  CHECK(b.first() == a);
+  
+  // Remove first - change
+  b.erase(b.begin());
+  CHECK(b.first() == c);
+  
+  // Another
+  b.clear();
+  CHECK_THROW(b.first(), std::logic_error);
+}
+
+TEST(access_last){
+  Node a(1),c(2);
+  Branch::id_type id{0,0,1};
+  Branch b{id,1};  
+  
+  // Try to access first -> error
+  CHECK_THROW(b.last(), std::logic_error);
+  
+  // Add node (cp)
+  b.push_back(a);
+  CHECK(b.last() == a);
+  
+  // add another node -  change
+  b.push_back(c);
+  CHECK(b.last() == c);
+  
+  // Remove first - dont change
+  b.erase(b.begin());
+  CHECK(b.last() == c);
+  
+  // Another
+  b.clear();
+  CHECK_THROW(b.last(), std::logic_error);
+}
+
+TEST(push_back) {
+  Node a(2);
+  Branch::id_type id{0,0,0};
+  Branch b{id,1};
   
   // Copy pushback
   b.push_back(a);
@@ -52,7 +191,7 @@ TEST(branch_push_back) {
   CHECK(b.last()==Node(3));
 }
 
-TEST(branch_insert) {
+TEST(insert) {
   Node r(1),a(2),c(3),d(4),e(5),f(6);
   std::vector<Node> nodes{a,c};
   Branch::id_type id{0,0,1};
@@ -71,7 +210,7 @@ TEST(branch_insert) {
   CHECK( *(++b.begin())==f);
 }
 
-TEST(branch_equality_op) {
+TEST(equality_op) {
   Node r(1),a(2),c(3),d(4),e(5);
   std::vector<Node> nodes_1{a,c};
   std::vector<Node> nodes_2{a,c,d};
@@ -105,3 +244,357 @@ TEST(branch_equality_op) {
   // Equal
   CHECK( b_1 == b_1);
 }
+
+TEST(erase_single){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK(b.first() == a);
+  CHECK(b.size() == 5);
+  
+  // Remove first node
+  b.erase(b.begin());
+  CHECK(b.size() == 4);
+  CHECK(b.first() == c);
+  
+  // Remove node in the middle
+  b.erase(std::next(b.begin(),1));
+  CHECK(b.size() == 3);
+  CHECK(*std::next(b.begin(),1) == e);
+  
+  // Remove last
+  b.erase(std::prev(b.end(),1));
+  CHECK(b.size() == 2);
+  CHECK(b.last() == e);
+}
+
+TEST(erase_single_end){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK(b.first() == a);
+  CHECK(b.size() == 5);
+  
+  // Remove empty
+  b.erase(b.end());
+  CHECK(b.size() == 5);
+  CHECK(b.first() == a);
+}
+
+TEST(erase_range_begin){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK(b.first() == a);
+  CHECK(b.size() == 5);
+  
+  // Remove first two nodes
+  b.erase(b.begin(), std::next(b.begin(),2));
+  CHECK(b.size() == 3);
+  CHECK(b.first() == d);
+}
+
+TEST(erase_range_end){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK(b.first() == a);
+  CHECK(b.size() == 5);
+  
+  // Remove first two nodes
+  b.erase(std::prev(b.end(),2), b.end());
+  CHECK(b.size() == 3);
+  CHECK(b.last() == d);
+}
+
+TEST(erase_range_mid){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK(b.first() == a);
+  CHECK(b.size() == 5);
+  
+  // Remove first two nodes
+  b.erase(std::next(b.begin(),1),std::prev(b.end(),1));
+  CHECK(b.size() == 2);
+  CHECK(b.last() == f);
+  CHECK(b.first() == a);
+}
+
+TEST(erase_range_empty){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK(b.first() == a);
+  CHECK(b.size() == 5);
+  
+  // Remove first two nodes
+  b.erase(b.begin(),b.begin());
+  CHECK(b.size() == 5);
+}
+
+// Clear fn
+
+TEST(clear){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK(b.first() == a);
+  CHECK(b.size() == 5);
+  
+  b.clear();
+  CHECK(b.size() == 0);
+  
+}
+
+TEST(clear_empty){
+  Branch::id_type id{0,0,1};
+  Branch b{id,1};
+  
+  CHECK(b.size() == 0);
+  b.clear();
+  CHECK(b.size() == 0);
+}
+
+
+// Split
+TEST(split_begin){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  Branch bs = b.split(b.begin());
+  
+  CHECK(b.size() == 1);
+  CHECK(bs.size() == 4);
+  CHECK(b.last() == bs.root());
+}
+
+TEST(split_end){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  Branch bs = b.split(b.end());
+  
+  CHECK(b.size() == 5);
+  CHECK(bs.size() == 0);
+  CHECK(!bs.has_root());
+}
+
+TEST(split_mid){
+  Node r(1),a(2),c(3),d(4),e(5),f(6);
+  std::vector<Node> nodes{a,c,d,e,f};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  Branch bs = b.split(std::next(b.begin(),2));
+  
+  CHECK(b.size() == 3);
+  CHECK(bs.size() == 2);
+  CHECK(b.first() == a);
+  CHECK(b.last() == d);
+  CHECK(bs.first() == e);
+  CHECK(bs.last() == f);
+  CHECK(bs.root() == b.last());
+}
+
+TEST(simplifiy_empty){
+  Branch::id_type id{0,0,1};
+  Branch b{id,1};
+  b.simplify(1);
+  CHECK(b.size() == 0);
+}
+
+TEST(simplifiy_single){
+  Node r(1),a(2);
+  std::vector<Node> nodes{a};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  b.simplify(1);
+  CHECK(b.size() == 1);
+}
+
+TEST(simplifiy_regular){
+  Node r(1,0,0,0,1),a(2,1,1,1,1),c(3,2,2,2,1),d(4,3,1,2,1);
+  std::vector<Node> nodes{a,c,d};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK(b.size() == 3);
+  b.simplify(0.1); // Should remove A (in the line r-c
+  CHECK(b.size() == 2);
+  CHECK(b.first() == c);
+  CHECK(b.last() == d);
+}
+
+TEST(scale_empty){
+  Node r(1,0,0,0,1);
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r};
+  
+  b.scale(1E6);
+  CHECK(neurostr::geometry::equal(r.position(),b.root().position()));
+}
+
+TEST(scale_unit){
+  Node r(1,0,0,0,1),a(2,1,1,1,1),c(3,2,2,2,1),d(4,3,1,2,1);
+  std::vector<Node> nodes{a,c,d};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  b.scale(1);
+  
+  CHECK(neurostr::geometry::equal(r.position(),b.root().position()));
+  
+  CHECK(neurostr::geometry::equal(a.position(),b.first().position()));
+  CHECK(neurostr::geometry::equal(d.position(),b.last().position()));
+}
+
+TEST(scale_half){
+  Node r(1,0,0,0,1),a(2,1,1,1,1),c(3,2,2,2,1),d(4,3,1,2,1);
+  std::vector<Node> nodes{a,c,d};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  b.scale(0.5);
+  
+  CHECK(neurostr::geometry::equal(r.position(),b.root().position()));
+  
+  CHECK_CLOSE(0, b.first().distance(point_type(0.5,0.5,0.5)), 1E-3);
+  CHECK_CLOSE(0, b.last().distance(point_type(1.5,0.5,1)), 1E-3);
+}
+
+TEST(scale_xyz_empty){
+  Node r(1,0,0,0,1);
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r};
+  
+  b.scale(1E6,1E6,0);
+  CHECK(neurostr::geometry::equal(r.position(),b.root().position()));
+}
+
+TEST(scale_xyz_unit){
+  Node r(1,0,0,0,1),a(2,1,1,1,1),c(3,2,2,2,1),d(4,3,1,2,1);
+  std::vector<Node> nodes{a,c,d};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  b.scale(1,1,1);
+  
+  CHECK_CLOSE(0,r.distance(b.root()), 1E-3);
+  CHECK_CLOSE(0,a.distance(b.first()), 1E-3);
+  CHECK_CLOSE(0,d.distance(b.last()), 1E-3);
+}
+
+TEST(scale_xyz){
+  Node r(1,0,0,0,1),a(2,1,1,1,1),c(3,2,2,2,1),d(4,3,1,2,1);
+  std::vector<Node> nodes{a,c,d};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  b.scale(2,1,0);
+  
+  CHECK_CLOSE(0,r.distance(b.root()), 1E-3);
+  CHECK_CLOSE(2,b.first().x(), 1E-3);
+  CHECK_CLOSE(1,b.first().y(), 1E-3);
+  CHECK_CLOSE(0,b.first().z(), 1E-3);
+  
+  CHECK_CLOSE(6,b.last().x(), 1E-3);
+  CHECK_CLOSE(1,b.last().y(), 1E-3);
+  CHECK_CLOSE(0,b.last().z(), 1E-3);
+}
+
+TEST(traslate_empty){
+  Branch::id_type id{0,0,1};
+  Branch b{id,1};
+  
+  b.traslate(point_type(1,1,1));
+}
+
+TEST(traslate_root){
+  Node r(1,0,0,0,1);
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r};
+  
+  b.traslate(point_type(1,1,1));
+  CHECK_CLOSE(0,b.root().distance(point_type(1,1,1)),1E-3);
+}
+
+TEST(traslate){
+  Node r(1,0,0,0,1),a(2,1,1,1,1),c(3,2,2,2,1),d(4,3,1,2,1);
+  std::vector<Node> nodes{a,c,d};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  b.traslate(point_type(1,1,1));
+  
+  CHECK_CLOSE(sqrt(3),r.distance(b.root()), 1E-3);
+  CHECK_CLOSE(sqrt(3),a.distance(b.first()), 1E-3);
+  CHECK_CLOSE(sqrt(3),d.distance(b.last()), 1E-3);
+}
+
+TEST(null_segments_empty){
+  Branch::id_type id{0,0,1};
+  Branch b{id,1};
+  
+  CHECK_EQUAL(b.size(),0);
+  b.remove_null_segments();
+  CHECK_EQUAL(b.size(),0);
+}
+
+TEST(null_segments_single){
+  Node r(1,0,0,0,1),a(2,0,0,0,0);
+  std::vector<Node> nodes{a};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK_EQUAL(b.size(),1);
+  b.remove_null_segments(); // Should remove a
+  CHECK_EQUAL(b.size(),0);
+}
+
+TEST(null_segments_single_no){
+  Node r(1,0,0,0,1),a(2,1,0,0,0);
+  std::vector<Node> nodes{a};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK_EQUAL(b.size(),1);
+  b.remove_null_segments();
+  CHECK_EQUAL(b.size(),1);
+}
+
+TEST(null_segments_set){
+  Node r(1,0,0,0,1),a(2,1,1,1,1),c(3,1,1,1,1),d(4,1,1,1,1);
+  std::vector<Node> nodes{a,c,d};
+  Branch::id_type id{0,0,1};
+  Branch b{id,1,r,nodes};
+  
+  CHECK_EQUAL(b.size(),3);
+  b.remove_null_segments(); // Remove everything but a
+  CHECK_EQUAL(b.size(),1);
+  CHECK_EQUAL(b.first(),a);
+}
+
+//TODO: Discrete frechet - need to test the geometry part first
+
+
+
+} // Branch tests
