3f5cc4f044cc36d3621dc475ec412d7a80ab95aa
Luis Rodriguez-Lujan <luis.rodriguezl@upm.es>
2016-10-18 12:29:08 +0200

    Added contour core class

---
+++ lib/include/core/contour.h
@@ -0,0 +1,173 @@
+#ifndef NEUROSTR_BASE_CONTOUR_H_
+#define NEUROSTR_BASE_CONTOUR_H_
+
+#include <string>
+#include <vector>
+
+#include "core/property.h"
+#include "core/geometry.h"
+
+namespace neurostr {
+  
+  /**
+ * @class Contour
+ * @author luis
+ * @date 18/10/16
+ * @file neuron.h
+ * @brief 
+ */
+class Contour{
+  
+  // Constructors
+  public:
+  
+    // convenience typedefs
+    using storage_type = std::vector<point_type>;
+    using iterator = storage_type::iterator;
+    using const_iterator = storage_type::const_iterator;
+    
+    /**
+     * @brief Contour empty constructor. Initializes all members to its default value
+     * and leaves the contour as empty
+    */
+    Contour();
+    
+    /**
+     * @brief Creates a contour from a point vector
+     * @param v Contour point vector
+     */
+    Contour( const std::vector<point_type>& v);
+    
+    // copy and move
+    Contour(const Contour&) = default;
+    Contour& operator=(const Contour&) = default;
+    
+    Contour(Contour&&) = default;
+    Contour& operator=(Contour&&) = default;
+  
+  // Data members
+  private: 
+  
+    // Contour name
+    std::string name_;
+    
+    // Points
+    std::vector<point_type> positions_;
+    
+    // Colors
+    std::string face_color_;
+    std::string back_color_;
+    float fill_;
+    
+    // Resolution
+    float resolution_;
+    
+    // Closed contour flag
+    bool closed_;
+  
+  // Member functions
+  public:
+    
+    // Point related functions
+    
+    /**
+     * @brief Returns an iterator to the first point in the contour
+     * @return point iterator
+     */
+    iterator begin();
+    const_iterator begin() const;
+    
+    /**
+     * @brief Returns an iterator to the next position after the last 
+     * point in the contour
+     * @return point iterator
+     */
+    iterator end();
+    const_iterator end() const;
+    
+    
+    /**
+     * @brief Contour size
+     * @return number of points that describe the contour
+     */
+    std::size_t size() const;
+    
+    /**
+     * @brief Computes the length of the contour
+     * @return Contour length
+     */
+    float length() const;
+    
+    /**
+     * @brief Contour planar area as 2D projection
+     * @return Contour planar area
+     */
+    float area() const;
+    
+    // Accessors
+    
+    /**
+     * @brief Returns contour name
+     * @return Contour name as string
+     */
+    const std::string& name() const;
+    
+    /**
+     * @brief Returns contour face and back color
+     * @return Color as hex string #RRGGBB
+     */
+    const std::string& face_color() const;
+    const std::string& back_color() const;
+    
+    
+    /**
+     * @brief Return contour fill density (alpha channel)
+     * @return Alpha (opacity) value
+     */
+    float fill_density() const;
+
+    /**
+     * @brief Contour resolution (im not sure what this actually is..like a ratio?)
+     * @return Resolution as float
+     */
+    float resolution() const;
+    
+    /**
+     * @brief True if the contour is defined as closed
+     * @return boolean flag
+     */
+    bool is_closed() const;
+    
+    /**
+     * @brief Set all contour properties getting them from a p. map
+     * @param p input Property map
+     */
+    void contour_properties_from_map(const PropertyMap& p);
+  
+  // Private methods
+  private:
+    
+    /**
+     * @brief Check the value type of the property
+     * @param p Property to check
+     */
+    static bool float_valued_( const PropertyMap::property_type& p);
+    static bool string_valued_( const PropertyMap::property_type& p);
+    static bool bool_valued_( const PropertyMap::property_type& p);
+    
+    
+    /**
+     * @brief Verify that the color string is correcty
+     * @param s Color string
+     */
+    static bool check_color_format_(const std::string& s);
+    
+    void set_from_property_(const PropertyMap::property_type& p);
+
+}; // End class contour
+
+} // end namespace neurostr
+
+
+
+#endif
---
+++ lib/src/core/contour.cpp
@@ -0,0 +1,146 @@
+#include <cctype>
+
+#include "core/contour.h"
+
+namespace neurostr {
+  
+  
+    
+    /**
+     * @brief Contour empty constructor. Initializes all members to its default value
+     * and leaves the contour as empty
+    */
+    Contour::Contour()
+      : name_()
+      , positions_()
+      , face_color_("#FFFFFF")
+      , back_color_("#000000")
+      , fill_(0.0)
+      , resolution_(1.0)
+      , closed_(false){};
+    
+    /**
+     * @brief Creates a contour from a point vector
+     * @param v Contour point vector
+     */
+    Contour::Contour( const std::vector<point_type>& v)
+      : name_()
+      , positions_(v)
+      , face_color_("#FFFFFF")
+      , back_color_("#000000")
+      , fill_(0.0)
+      , resolution_(1.0)
+      , closed_(false){};
+    
+    // Point related functions
+    
+    Contour::iterator Contour::begin(){ return positions_.begin(); }
+    Contour::const_iterator Contour::begin() const { return positions_.begin(); };
+    
+    Contour::iterator Contour::end() { return positions_.end(); };
+    Contour::const_iterator Contour::end() const { return positions_.end(); };
+    
+
+    std::size_t Contour::size() const { return positions_.size(); };
+    
+
+    float Contour::length() const {
+      if(size() <= 1) return 0;
+      else {
+        float len = 0.0;
+        for(auto it = std::next(begin(),1); it != end(); ++it ){
+          // Add distance bw every two consecutive points
+          len += geometry::distance( *std::prev(it,1), *it);
+        }
+        // Add one more if it is closed
+        if(closed_) {
+          len += geometry::distance( *std::prev(end(),1), *begin() );
+        }
+        return len;
+      }
+    }
+    
+    float Contour::area() const {
+       if(size() <= 1) return 0;
+       else return NAN; // TODO: NOT IMPLEMENTED YET
+    }
+    
+    const std::string& Contour::name() const { return name_; }
+    
+
+    const std::string& Contour::face_color() const { return face_color_; }
+    const std::string& Contour::back_color() const { return back_color_; }
+    
+    
+
+    float Contour::fill_density() const {return fill_; }
+
+   
+    float Contour::resolution() const { return resolution_;}
+    
+   
+    bool Contour::is_closed() const { return closed_;}
+    
+    
+    void Contour::contour_properties_from_map(const PropertyMap& p){
+      // Check every property in the map
+      for(auto it = p.begin(); it != p.end() ; ++it){
+        set_from_property_(*it);
+      }
+    }
+  
+  
+    bool Contour::float_valued_( const PropertyMap::property_type& p) {
+      return PropertyMap::is<float>(p);
+    }
+    
+    bool Contour::string_valued_( const PropertyMap::property_type& p){
+      return PropertyMap::is<std::string>(p);  
+    }
+    
+    bool Contour::bool_valued_( const PropertyMap::property_type& p){
+      return PropertyMap::is<bool>(p);
+    }
+    
+    bool Contour::check_color_format_(const std::string& s){
+      if( s.size()!= 7 ) return false;
+      else {
+        if( s[0] != '#') return false;
+        else {
+          for(std::size_t i = 1; i < s.size() ; ++i){
+            if( !std::isxdigit(s[i]) ) return false;
+          }
+          return true;
+        }
+      }
+    }
+    
+    void Contour::set_from_property_(const PropertyMap::property_type& p){
+      
+      std::string name = PropertyMap::key(p);
+      
+      if( name == std::string("back_color") ){
+        if( string_valued_(p) )
+          back_color_ = PropertyMap::value<std::string>(p);
+      } else if( name == std::string("face_color") ){
+        if( string_valued_(p) )
+          face_color_ = PropertyMap::value<std::string>(p);
+      } else if( name == std::string("closed") ){
+        if( bool_valued_(p) )
+          closed_ = PropertyMap::value<bool>(p);
+      } else if( name == std::string("filldensity") ){
+        if( float_valued_(p) )
+          fill_ = PropertyMap::value<float>(p);
+      } else if( name == std::string("name") ){
+        if( string_valued_(p) )
+          name_ = PropertyMap::value<std::string>(p);
+      } else if( name == std::string("resolution") ){
+        if( float_valued_(p) )
+          resolution_ = PropertyMap::value<float>(p);
+      }
+    }
+
+
+} // end namespace neurostr
+
+
