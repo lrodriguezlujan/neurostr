a7ac1ac6818b1238d84a2f396e1548baa1000f7f
Luis Rodriguez-Lujan <luis.rodriguezl@upm.es>
2016-10-28 14:52:11 +0200

    Added type check to JSON parser - Fixes #83

--- lib/src/io/JSONParser.cpp
+++ lib/src/io/JSONParser.cpp
@@ -1,13 +1,28 @@
 #include "io/JSONParser.h"
 #include "rapidjson/error/en.h"
+#include "core/log.h"
 
 namespace neurostr {
 namespace io {
   
   point_type JSONParser::parsePoint(const rapidjson::Value::ConstObject& v) {
-    //assert(v.HasMember("x") && v["x"].IsNumber());
-    //assert(v.HasMember("y") && v["y"].IsNumber());
-    //assert(v.HasMember("z") && v["z"].IsNumber());
+    
+    if( !v.HasMember("x") ){
+      throw std::logic_error("Missing field 'x' in point");
+    } else if( !v.HasMember("y") ){
+      throw std::logic_error("Missing field 'y' in point");
+    } else if( !v.HasMember("y") ){
+      throw std::logic_error("Missing field 'y' in point");
+    }
+    
+    // Check types
+    if( !v["x"].IsNumber() ){
+      throw std::logic_error("Point field 'x' is not numeric");
+    } else if( !v["y"].IsNumber() ){
+      throw std::logic_error("Point field 'y' is not numeric");
+    } else if( !v["z"].IsNumber() ){
+      throw std::logic_error("Point field 'z' is not numeric");
+    }
     
     return point_type( v["x"].GetFloat(), 
                        v["y"].GetFloat(), 
@@ -23,13 +38,15 @@ namespace io {
       return PropertyMap::property_type(name, boost::any() );
     else if (m.value.IsInt()) {
       return PropertyMap::property_type(name, boost::any(m.value.GetInt()));
-    } else if (m.value.IsFloat()) {
+    } else if (m.value.IsNumber()) {
       return PropertyMap::property_type(name, boost::any(m.value.GetFloat()));
     } else if (m.value.IsString()) {
       return PropertyMap::property_type(name, boost::any(m.value.GetString()));
     } else if (m.value.IsObject()) {
       return PropertyMap::property_type(name, boost::any(parsePoint(m.value.GetObject())));
     } else {
+      NSTR_LOG_(warn, std::string("Unrecognized property type for ") + name);
+      ++warn_count;
       return PropertyMap::property_type(name, boost::any() );
     }
   }
@@ -53,13 +70,32 @@ namespace io {
     //assert(v.HasMember("id") && v["id"].IsInt());
     //assert(v.HasMember("r") && v["r"].IsNumber());
     
+    if(!v.HasMember("id")){
+      throw std::logic_error("Missing node ID");
+    } else if( !v["id"].IsInt() ){
+      throw std::logic_error("Node ID is not an integer");
+    }
+    
+    if(!v.HasMember("r")){
+      throw std::logic_error("Missing node radius");
+    } else if( !v["id"].IsNumber() ){
+      throw std::logic_error("Node radius is not numeric");
+    }
+    
     // Parse
+    
+    // We Dont catch parse point error - Propagate them 
     Node n( v["id"].GetInt(), 
             parsePoint(v),
             v["r"].GetFloat());
     // Parse properties
     if(v.HasMember("properties")){
-      n.properties = parseProperties(v["properties"].GetObject());
+      if(!v["properties"].IsObject() ){
+        NSTR_LOG_(warn,"Node properties field is not an object");
+        ++warn_count;
+      } else {
+        n.properties = parseProperties(v["properties"].GetObject());
+      }
     }
     return n;
     
@@ -75,23 +111,55 @@ namespace io {
     //assert(v.HasMember["root"] && v["root"].IsObject());
     //assert(v.HasMember["nodes"] && v["nodes"].IsArray());
     
-    // Root
-    pos->root( parseNode(v["root"].GetObject()) );
+    // Branch has root
+    if(!v.HasMember("root")){
+      ++warn_count;
+      NSTR_LOG_(warn, "Unrooted branch");
+    } else {
+      // Root (may throw error)
+      try{
+        pos->root( parseNode(v["root"].GetObject()) );  
+      } catch( std::logic_error e){
+        // We kjust ignore the root
+        process_error(e);
+        NSTR_LOG_(info, "Error while parsing branch root. Skipped.");  
+        pos->remove_root();  
+      }
+    }
+    
+    if(!v.HasMember("nodes")){
+      throw std::logic_error("Missing nodes field in branch");
+    } else if(!v["nodes"].IsArray()){
+      throw std::logic_error("Branch nodes field is not an array");
+    }
     
     // nodes
     auto tmp = v["nodes"].GetArray();
     pos->clear();
     for(auto it = tmp.begin() ; it != tmp.end() ; ++it){
-      pos->push_back( parseNode(it->GetObject()));
+      try{
+        pos->push_back( parseNode(it->GetObject()));
+      } catch( std::logic_error e){
+        process_error(e);
+        NSTR_LOG_(info, "Conflicting nodes will are omitted");  
+      }
     }
     
     // Properties
      if(v.HasMember("properties")){
-      pos->properties = parseProperties(v["properties"].GetObject());
+      if(!v["properties"].IsObject() ){
+        NSTR_LOG_(warn,"Branch properties field is not an object - Ignoring");
+        ++warn_count;
+      } else {
+        pos->properties = parseProperties(v["properties"].GetObject());
+      }
     }
     
     // Children
      if(v.HasMember("children")){
+      if(!v["nodes"].IsArray()){
+          throw std::logic_error("Branch children field is not an array");
+      }
        //assert(v["children"].IsArray());
        auto tmp = v["children"].GetArray();
        for(auto it = tmp.begin() ; it != tmp.end() ; ++it){
@@ -101,7 +169,12 @@ namespace io {
                         .append_branch(pos,Branch()); // This copies neurite in b.
          
          // Parse recursive
-         parseBranch(it->GetObject(), newpos);
+         try{
+          parseBranch(it->GetObject(), newpos);
+         } catch(std::logic_error e){
+           process_error(e);
+           NSTR_LOG_(info, "Ignoring conflicting branch");  
+         }
        }
      }                  
   }
@@ -111,13 +184,35 @@ namespace io {
     //assert(v.HasValue("id") && v["id"].IsUint() );
     //assert(v.HasValue("type") && v["type"].IsUint()) );
     //assert(v.HasValue("tree") && v["tree"].IsObject()) );
+    if(!v.HasMember("id")){
+      throw std::logic_error("Missing neurite id field");
+    } else if ( !v["id"].IsUint()){
+      throw std::logic_error("Neurite id field is not an unsigned integer");
+    }
+    
+    if(!v.HasMember("type")){
+      throw std::logic_error("Missing neurite type field");
+    } else if ( !v["type"].IsUint()){
+      throw std::logic_error("Neurite type field is not an unsigned integer");
+    }
+    
+    if(!v.HasMember("tree")){
+      throw std::logic_error("Missing neurite tree field");
+    } else if ( !v["tree"].IsObject()){
+      throw std::logic_error("Neurite tree field is not an object");
+    }
     
     // Create neurite
     Neurite * n = new Neurite(v["id"].GetUint(), NeuriteType(v["type"].GetUint()) );
     
     // Properties..
     if(v.HasMember("properties")){
-      n->properties = parseProperties(v["properties"].GetObject());
+      if(!v["properties"].IsObject() ){
+        NSTR_LOG_(warn,"Neurite properties field is not an object - Ignoring");
+        ++warn_count;
+      } else {
+        n->properties = parseProperties(v["properties"].GetObject());
+      }
     }
     
     // Tree
@@ -131,34 +226,72 @@ namespace io {
     //assert(v.HasValue("id") && v["id"].IsString() );
     //assert(v.HasValue("neurites") && v["neurites"].IsArray()) );
     
+    if(!v.HasMember("id")){
+      throw std::logic_error("Missing neuron id field");
+    } else if ( !v["id"].IsString()){
+      throw std::logic_error("Neuron id field is not a string");
+    }
+    
     Neuron* n = new Neuron(v["id"].GetString());
     
     // If has soma
     if(v.HasMember("soma")){
       //assert(v["soma"].IsObject());
       //assert(v["soma"].HasValue("nodes"));
-
-      // Get soma nodes
-      auto tmp = v["soma"]["nodes"].GetArray();
-
-      std::vector<Node> soma(tmp.Size());
       
-      // Parse each node
-      for( auto it = tmp.begin(); it != tmp.end() ; ++it){
-        soma.push_back(parseNode(it->GetObject()));
+      if(!v["soma"].IsObject()){
+        NSTR_LOG_(warn,"Neuron soma is not an object - Skipping");
+        ++warn_count;
+      } else if (!v["soma"].HasMember("nodes")) {
+        NSTR_LOG_(warn,"Missing nodes field in soma - Skipping");
+        ++warn_count;
+      } else if ( !v["soma"]["nodes"].IsArray() ){
+        NSTR_LOG_(warn,"Soma nodes is not an array - Skipping");
+        ++warn_count;
+      } else {
+        // Get soma nodes
+        auto tmp = v["soma"]["nodes"].GetArray();
+
+        std::vector<Node> soma(tmp.Size());
+        
+        // Parse each node
+        for( auto it = tmp.begin(); it != tmp.end() ; ++it){
+          try{
+            soma.push_back(parseNode(it->GetObject()));
+          } catch (std::logic_error e){
+            process_error(e);
+            NSTR_LOG_(info, "Conflicting soma nodes will be ignored");
+          }
+        }
+        n->add_soma(soma);
       }
-      n->add_soma(soma);
     }
     
     // Properties..
     if(v.HasMember("properties")){
-      n->properties = parseProperties(v["properties"].GetObject());
+      if(!v["properties"].IsObject() ){
+        NSTR_LOG_(warn,"Neuron properties field is not an object - Ignoring");
+        ++warn_count;
+      } else {
+        n->properties = parseProperties(v["properties"].GetObject());
+      }
+    }
+    
+    if(!v.HasMember("neurites")){
+      throw std::logic_error("Missing neurites field in Neuron");
+    } else if ( !v["id"].IsArray()){
+      throw std::logic_error("Neuron neurites field is not an array");
     }
     
     // Tree
     auto tmp = v["neurites"].GetArray();
     for(auto it = tmp.begin() ; it != tmp.end() ; ++it){
-      n->add_neurite(parseNeurite(it->GetObject()));
+      try{
+        n->add_neurite(parseNeurite(it->GetObject()));
+      } catch (std::logic_error e){
+        process_error(e);
+        NSTR_LOG_(info, "Conflicting neurites are ignored");
+      }
     }
     
     return n;
@@ -173,17 +306,32 @@ namespace io {
     
     // Check if it is a single neuron or a neuron set
     if(doc.HasMember("neurons")){
+      if(!doc["neurons"].IsArray()){
+        throw std::logic_error("neurons field is not an array");
+      }
+      
       // Is a rec. set
       auto tmp = doc["neurons"].GetArray();
       for(auto it = tmp.Begin() ; it != tmp.End() ; ++it){
-        r->addNeuron(parseNeuron(it->GetObject()));
+        try{
+          r->addNeuron(parseNeuron(it->GetObject()));
+        } catch (std::logic_error e){
+          process_error(e);
+          NSTR_LOG_(info, "Erroneous neurons are ignored");
+        }
       } 
       
       // Read contour
       // TODO
     } else {
       // Single
-      r->addNeuron(parseNeuron(doc.GetObject()));
+      
+      try{
+        r->addNeuron(parseNeuron(doc.GetObject()));
+      } catch (std::logic_error e){
+          process_error(e);
+          NSTR_LOG_(info, "Erroneous neurons are ignored");
+      }
     }
     
     return std::unique_ptr<Reconstruction>(r);
@@ -205,10 +353,22 @@ namespace io {
       return std::unique_ptr<Reconstruction>( new Reconstruction(name) );
     }
     //assert(doc.IsObject());
-
+    std::unique_ptr<Reconstruction> ret;
+    try{
+      ret = parse_document(name,doc);
+      
+    } catch(std::exception e){
+      critical_error = true;
+      NSTR_LOG_(critical, e.what());
+      ++error_count;
+    }
     
-    return parse_document(name,doc);
-
+    if(error_count > 0){
+      NSTR_LOG_(warn, std::to_string(error_count) + 
+      " were detected while processing the file. Please, check the file and correct the errors.")
+    }
+    
+    return ret;
   }
   
 } // IO NS end
